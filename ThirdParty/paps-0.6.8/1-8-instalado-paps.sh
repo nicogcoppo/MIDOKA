04a69740dabf4415489f9cf2c7caac4018b1cdc0
nico <nico@b590-debian92-x64.b590-debian92-x64>
2019-01-08 11:44:24 -0300

    paps instalacion

--- ThirdParty/paps-0.6.8/1-8-instalado-paps.sh
+++ ThirdParty/paps-0.6.8/1-8-instalado-paps.sh
@@ -6,15 +6,17 @@
 # precvios
 
 
-function instalado {
-
-    make install && install -v -m755 -d /usr/share/doc/paps-0.6.8 && install -v -m644 doxygen-doc/html/* /usr/share/doc/paps-0.6.8
-
-}
-
 ./configure --prefix=/usr --mandir=/usr/share/man && make
 
-sudo bash 1-8-instalado-paps-ROOT.sh && echo "INSTALACION EXITOSA" || echo "FALLO LA INSTALACION"
+sudo -s <<EOF
+
+apt-get install libpango1.0-dev
+
+make install && install -v -m755 -d /usr/share/doc/paps-0.6.8 && install -v -m644 doxygen-doc/html/* /usr/share/doc/paps-0.6.8
+
+EOF
+
+
 
 
 
