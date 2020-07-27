e5d2f95b233a005ec1fd8ca94e74431746ba0e67
nico <nico@debianThinkpadx230.debianThinkpadx230>
2020-07-27 14:42:28 -0300

    analisis Costos

--- scripts/informeContable.sh
+++ scripts/informeContable.sh
@@ -15,11 +15,15 @@ Lsup=$2
 
 resultados=$RANDOM
 
+costos=$RANDOM
+
 ############## SCRIPT #################################
 
 echo -e "\nPASSWORD... " 
 
 read pass
+
+###--- ANALISIS ECONOMICO -- ###
 
 cat ./scripts/scriptSql/informeContable.sql | sed "s/Linf/${Linf}/g" | sed "s/Lsup/${Lsup}/g" >/tmp/informeContable.sql
 
@@ -28,6 +32,17 @@ test -f "/tmp/${resultados}" || mysql -u "${user}" --password="${pass}" </tmp/in
 mysql -u "${user}" --password="${pass}" </tmp/informeContable.sql | tail -n +2 | tr '\t' ';' | tr '.' ',' >> /tmp/${resultados}
 
 cp /tmp/${resultados} ./informeContable_$RANDOM.csv
+
+###--- DETALLE COSTOS -- ###
+
+cat ./scripts/scriptSql/detalleCostos.sql | sed "s/Linf/${Linf}/g" | sed "s/Lsup/${Lsup}/g" >/tmp/detalleCostos.sql
+
+test -f "/tmp/${costos}" || mysql -u "${user}" --password="${pass}" </tmp/detalleCostos.sql | head -1 | tr '\t' ';' > /tmp/${costos}  # Encabezado
+
+mysql -u "${user}" --password="${pass}" </tmp/detalleCostos.sql | tail -n +2 | tr '\t' ';' | tr '.' ',' >> /tmp/${costos}
+
+cp /tmp/${costos} ./detalleCostos_$RANDOM.csv
+
 
 
 ############## MANTENIMIENTO #############
---
+++ scripts/scriptSql/detalleCostos.sql
@@ -0,0 +1,26 @@
+USE MIDOKA_PGC_B;
+SELECT SOLICITUDES.REALIZACION as 'FECHA',opID AS 'REMITO',DENOMINACION AS 'CLIENTE',CONCAT(clasificacion_cla," ",mod_md," ",motivo_mt) AS 'ARTICULO',(CANTIDAD_AR-IF(OPERACIONES.SOLICITUD=42, CANTIDAD_ING, 0))*CANTIDAD_MINIMA AS 'CANTIDAD',t1.PRECIO*(1 - IFNULL(DESCUENTO, 0) / 100) AS 'COSTO',(CANTIDAD_AR-IF(OPERACIONES.SOLICITUD=42, CANTIDAD_ING, 0))*CANTIDAD_MINIMA*t1.PRECIO*(1 - IFNULL(DESCUENTO, 0) / 100) AS 'SUBTOTAL',IF(DENOMINACION IS NULL,0,ROUND(IF(CONDICION = CONDICION_REFERENCIA AND DESCUENTO_ESPECIAL <> 0,(1 - (DESCUENTO_ESPECIAL/100)) * IF(VALOR_AGREGADO>'0',(1 + VALOR_AGREGADO/100) * t1.PRECIO,t1.PRECIO) * IF(IFNULL(CANTIDAD_AR,0)>IFNULL(CANTIDAD_ING,0),CANTIDAD_AR * CANTIDAD_MINIMA,(-1 * CANTIDAD_ING) * CANTIDAD_MINIMA),IF(DESCUENTO_ESPECIFICO > '0',(1 - DESCUENTO_ESPECIFICO/100 ) * IF(VALOR_AGREGADO>'0',(1 + VALOR_AGREGADO/100) * t1.PRECIO,t1.PRECIO) * IF(IFNULL(CANTIDAD_AR,0)>IFNULL(CANTIDAD_ING,0),CANTIDAD_AR * CANTIDAD_MINIMA,(-1 * CANTIDAD_ING) * CANTIDAD_MINIMA),cond_num_cd * IF(VALOR_AGREGADO>'0',(1 + VALOR_AGREGADO/100) * t1.PRECIO,t1.PRECIO) * IF(IFNULL(CANTIDAD_AR,0)>IFNULL(CANTIDAD_ING,0),CANTIDAD_AR * CANTIDAD_MINIMA,(-1 * CANTIDAD_ING) * CANTIDAD_MINIMA))),2)) AS 'FACTURADO'
+   FROM ARMADO JOIN(PRECIOS, CATEGORIA, MODELO, CONDICION, ARTICULOS, CLIENTE, OPERACIONES, PROOVEDOR, SOLICITUDES,MOTIVO) ON(PRECIOS.RUBRO_pr=ARTICULOS.RUBRO
+                                                                                                          AND PRECIOS.CATEGORIA_pr=ARTICULOS.CATEGORIA
+                                                                                                          AND PRECIOS.MODELO_pr=ARTICULOS.MODELO
+                                                                                                          AND CATEGORIA.claID=ARTICULOS.CATEGORIA
+                                                                                                          AND MODELO.mdID=ARTICULOS.MODELO
+                                                                                                          AND CONDICION.cdID=CLIENTE.CONDICION
+                                                                                                          AND CLIENTE.clID=OPERACIONES.INTERESADO
+                                                                                                          AND ARTICULOS.artID=ARMADO.ID_ARTICULO_AR
+                                                                                                          AND OPERACIONES.opID=ARMADO.OPERACION_REFERENCIA_AR
+                                                                                                          AND PROOVEDOR.pooID=PRECIOS.PROOVEDOR
+                                                                                                          AND opID=SOLICITUDES.REFERENCIA
+                                                                                                          AND mtID=ARTICULOS.MOTIVO)
+   JOIN PRHISTORICOS t1 ON(PRECIOS.RUBRO_pr=t1.RUBRO
+                           AND PRECIOS.CATEGORIA_pr=t1.CATEGORIA
+                           AND PRECIOS.MODELO_pr=t1.MODELO
+                           AND t1.VIGENCIA<=SOLICITUDES.REALIZACION)
+   LEFT JOIN PRHISTORICOS t2 ON(t1.RUBRO=t2.RUBRO
+                                AND t1.CATEGORIA=t2.CATEGORIA
+                                AND t1.MODELO=t2.MODELO
+                                AND t2.VIGENCIA > t1.VIGENCIA
+                                AND t2.VIGENCIA <=SOLICITUDES.REALIZACION)
+   WHERE t2.VIGENCIA IS NULL
+   	 AND CLASE=9
+	 AND SOLICITUDES.REALIZACION>'Linf' AND SOLICITUDES.REALIZACION<='Lsup' ORDER BY opID,CONCAT(clasificacion_cla," ",mod_md," ",motivo_mt);
